2af3270dd668aed22d1ee1b039dbdf1136b7f83b
vega.zhang.it@gmail.com <vega.zhang.it@gmail.com@8316f312-1370-fc0d-bed8-378d8c307916>
2013-02-12 09:52:38 +0000

    Vega add

---
+++ trunk/MiBo/MiBo/pages/administer/index.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="Bảng điều khiển" Language="C#" MasterPageFile="~/pages/common/administer.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="MiBo.pages.administer.index" %>
+<asp:Content ID="ContentHead" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="ContentMain" ContentPlaceHolderID="main" runat="server">
+</asp:Content>
--- trunk/MiBo/MiBo/pages/common/administer.Master.cs
+++ trunk/MiBo/MiBo/pages/common/administer.Master.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MiBo.Domain.Common.Controller;
 
 namespace MiBo.pages.common
 {
-    public partial class administer : System.Web.UI.MasterPage
+    public partial class administer : BaseMasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void lnkSignOut_Command(object sender, CommandEventArgs e)
+        {
+            Session["UserId"] = null;
+            Session["UserName"] = null;
+            Session["LocaleId"] = null;
+            //Response.Redirect(Pages.ADMIN_USERS_LOGIN);
         }
     }
 }
